19627b50836625393c1a6b33e0138533df764680
Alphy Gacheru <alphygacheru@gmail.com>
2022-08-22 16:49:33 +0300

    Create todos state and render in the TodoContainer.js

---
+++ src/components/TodoContainer.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+class TodoContainer extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      todos: [
+        {
+          id: 1,
+          title: 'Setup development environment',
+          completed: true,
+        },
+        {
+          id: 2,
+          title: 'Develop website and add content',
+          completed: false,
+        },
+        {
+          id: 3,
+          title: 'Deploy to live server',
+          completed: false,
+        },
+      ],
+    };
+  }
+
+  render() {
+    const { todos } = this.state;
+    return (
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>{todo.title}</li>
+        ))}
+      </ul>
+    );
+  }
+}
+export default TodoContainer;

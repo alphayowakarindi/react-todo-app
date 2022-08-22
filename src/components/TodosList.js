7ff68b265f85c6aade7805ad1843e07e9c097980
Alphy Gacheru <alphygacheru@gmail.com>
2022-08-22 18:12:38 +0300

    Pass todo from ToDoList.js to TodoItem.js

--- src/components/TodosList.js
+++ src/components/TodosList.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import TodoItem from './TodoItem';
 
 // eslint-disable-next-line react/prefer-stateless-function
 class TodosList extends React.Component {
@@ -9,7 +10,7 @@ class TodosList extends React.Component {
     return (
       <ul>
         {todos.map((todo) => (
-          <li key={todo.id}>{todo.title}</li>
+          <TodoItem key={todo.id} todo={todo} />
         ))}
       </ul>
     );
